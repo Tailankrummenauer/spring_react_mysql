60314325c78753c9f2b28eca961316b7af88b1c9
Tailankrummenauer <tailankrummenauer@gmail.com>
2023-04-17 17:45:19 -0300

    atualizando DELETE e ALTERAR

--- src/main/java/br/com/api/produtos/controle/ProdutoControle.java
+++ src/main/java/br/com/api/produtos/controle/ProdutoControle.java
@@ -2,27 +2,45 @@ package br.com.api.produtos.controle;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.api.produtos.modelo.ProdutoModelo;
+import br.com.api.produtos.modelo.RespostaModelo;
 import br.com.api.produtos.servico.ProdutoServico;
 
 @RestController //Spring ja sabe que é o arquivo responsavel por criar rotas
 
-
+@CrossOrigin(origins = "*") //--> deixa qualquer porta acessar as requisições 
 public class ProdutoControle {
 
     @Autowired ProdutoServico ps;
 
 
+    @DeleteMapping("/delete/{codigo}")
+    public ResponseEntity<RespostaModelo> remover(@PathVariable Long codigo){
+        return ps.remover(codigo);
+    }
+
     //--> metodo responsavel para cadastrar produtos
     @PostMapping("/cadastrar")
-    public ResponseEntity<?> cadastrar(@RequestBody ProdutoModelo pm){
-        return ps.cadastrar(pm);
+    public ResponseEntity<?> cadastrar(@RequestBody ProdutoModelo pm){ //cria um objeto produto modelo PM
+        return ps.cadastrarAlterar(pm, "cadastrar");
     }
+
+
+    //--> metodo responsavel para alterar produtos
+    @PutMapping("/alterar")
+    public ResponseEntity<?> alterar(@RequestBody ProdutoModelo pm){
+        return ps.cadastrarAlterar(pm, "alterar");
+    }
+    
 
     //--> metodo para listar os produtosModelos
     @GetMapping("/listar")
--- src/main/java/br/com/api/produtos/servico/ProdutoServico.java
+++ src/main/java/br/com/api/produtos/servico/ProdutoServico.java
@@ -27,8 +27,8 @@ public class ProdutoServico {
         return pr.findAll();
     }
 
-    // --> metodo responsavel para cadastrar produtos
-    public ResponseEntity<?> cadastrar(ProdutoModelo pm) {
+    // --> metodo responsavel para cadastrar ou alterar produtos
+    public ResponseEntity<?> cadastrarAlterar(ProdutoModelo pm ,  String acao) { //esse parametro diz que o metodo ta returnando httpStatus.created ou ok
         if (pm.getNome().equals("")) {
             rm.setMensagem("O nome do produto é obrigatorio");
             return new ResponseEntity<RespostaModelo>(rm, HttpStatus.BAD_REQUEST);
@@ -37,9 +37,22 @@ public class ProdutoServico {
             rm.setMensagem("O nome da marca é obrigatorio");
             return new ResponseEntity<RespostaModelo>(rm, HttpStatus.BAD_GATEWAY);
         }else{
-            return new ResponseEntity<ProdutoModelo>(pr.save(pm), HttpStatus.CREATED);
+            if(acao.equals("cadastrar")){
+                return new ResponseEntity<ProdutoModelo>(pr.save(pm), HttpStatus.CREATED);
+            }else{
+                return new ResponseEntity<ProdutoModelo>(pr.save(pm), HttpStatus.OK);
+                
+            }
         }
 
+    }
+
+    //--> metodo para remover produtos
+
+    public ResponseEntity<RespostaModelo> remover(Long codigo){
+        pr.deleteById(codigo);
+        rm.setMensagem("O produto foi removido com sucesso");
+        return new ResponseEntity<RespostaModelo>(rm, HttpStatus.OK);
     }
 
 }
